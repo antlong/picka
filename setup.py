77e53b7d854419cca6b75c86e35d12ef256da6ea
antlong <antlong@gmail.com>
2014-01-07 18:07:19 -0500

    Fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='picka',
-    version='0.9.3',
+    version='0.9.4',
     description='Picka generates data for use in any application.',
     author='Anthony Long',
     author_email='antlong@gmail.com',
