21b1ac414e2601ce34fbe2e67fa68bdfe9b9f2f0
Anthony Long <anthony@appcard.com>
2014-01-17 12:30:11 -0500

    Added picka.image, picka.rbg, picka.rbga

--- picka/picka.py
+++ picka/picka.py
@@ -767,3 +767,35 @@ def mac_address():
         random.randint(0x00, 0xff)
     ]
     return ':'.join(map(lambda x: "%02x" % x, mac))
+
+
+def rbg():
+    x = [random.randrange(0, 256) for i in xrange(3)]
+    return tuple(x)
+
+def rbga(r=None, b=None, g=None, a=None):
+    x = [random.randrange(0, 256) for i in xrange(4)]
+    return tuple(x)
+
+
+def image(filepath, length=100, width=100):
+    """Generate a random colored image, with random text on it.
+    Returns filepath for ease of use.
+
+        :param: filepath: path to save image to.
+        :type filepath: str
+        :param length: length of the image
+        :type length: int
+        :param width: width of the image
+        :type width: int
+    """
+    try:
+        import Image, ImageDraw
+    except ImportError as e:
+        print e, "Please install PIL to use this functionality."
+    im = Image.new('RGBA', tuple((length, width)), tuple((rbga(a=255))))
+    draw = ImageDraw.Draw(im)
+    text = sentence_actual(1)
+    draw.text((0, 0), text, fill=rbg())
+    im.save(filepath)
+    return filepath
