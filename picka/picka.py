89aab49ef24f57bdd2f717c347524921c7955451
Anthony Long <anthony@appcard.com>
2014-01-17 14:09:58 -0500

    Added hex_color

--- picka/picka.py
+++ picka/picka.py
@@ -799,3 +799,26 @@ def image(filepath, length=100, width=100):
     draw.text((0, 0), text, fill=rbg())
     im.save(filepath)
     return filepath
+
+def hex_color():
+    def _chkarg(a):
+        if isinstance(a, int): # clamp to range 0--255
+            if a < 0:
+                a = 0
+            elif a > 255:
+                a = 255
+        elif isinstance(a, float): # clamp to range 0.0--1.0 and convert to integer 0--255
+            if a < 0.0:
+                a = 0
+            elif a > 1.0:
+                a = 255
+            else:
+                a = int(round(a*255))
+        else:
+            raise ValueError('Arguments must be integers or floats.')
+        return a
+    r,b,g = rbg()
+    r = _chkarg(r)
+    g = _chkarg(g)
+    b = _chkarg(b)
+    return '#{:02x}{:02x}{:02x}'.format(r,g,b)
