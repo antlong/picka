fb9cadda631311c6a36d4555d0279f3ebbc0f3cd
Anthony Long <antlong@gmail.com>
2015-05-14 19:22:03 -0400

    hex_color was actually broken

--- picka/colors.py
+++ picka/colors.py
@@ -10,21 +10,25 @@ query_single = _query.query_single
 
 
 def rgb():
+    # todo: change from string?
     return [str(randint(0, 256)) for _ in range(3)]
 
 
 def rgba():
+    # todo: expand tests
     r, g, b = rgb()
     a = 100 * float(randint(1, 256)) / float(256) / 100
     return AttrDict({"r": r, "g": g, "b": b, "a": "%.2f" % round(a, 2)})
 
 
 def hex_color():
+    # todo: add test
     s = ""
     for x in rgb():
-        s += hex(x)[2:4].zfill(2)
+        s += hex(int(x))[2:4].zfill(2)
     return s
 
 
 def html_name():
+    # todo: add test
     print query_multiple("name,hex", "html_colors")
